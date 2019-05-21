9ceecd7e4dcd098721d1dbb3225ec7762f53d2f7
yvolorenzo2019 <yvolorezo@gmail.com>
2019-05-22 04:12:20 +0900

    Project's Initial Commit

---
+++ www/js/banco.js
@@ -0,0 +1,23 @@
+$(document).on("click","#cadastrar",function(){
+    var parametros = {
+        "nome": $("#nome").val(),
+        "email": $("#email").val()
+    };
+
+    $.ajax({
+        type:"post", //como enviar
+        url:"https://mobile2i2-jussimar.c9users.io/webservice/cadastra.php",//para onde enviar
+        data:parametros,//o que enviar
+        //se der certo
+        success: function(data){
+            navigator.notification.alert(data);
+            $("#nome").val("");
+            $("#email").val("");
+
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });    
+});
